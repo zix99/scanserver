b7fff6f836aacca0955d15884e48464679962737
Christopher LaPointe <cmlapointe11@gmail.com>
2018-07-12 15:48:29 -0400

    Add more scan server code

--- server.py
+++ server.py
@@ -1,27 +1,82 @@
 #!/usr/bin/env python
-from bottle import route, run, template, post, response, request
+from bottle import route, run, template, post, response, request, redirect, abort, static_file
+from PIL import Image
+from StringIO import StringIO
 import subprocess
 import time
+import os
 
 PORT = 9012
+SCAN_DIR = 'scans/'
+
+if not os.path.isdir(SCAN_DIR):
+	os.mkdir(SCAN_DIR)
+
+page = 1
 
 @route('/')
 def scanpage():
-	return template('scan')
+	files = os.listdir(SCAN_DIR)
+	file_details = []
+	for f in files:
+		if f.endswith("jpg"):
+			stat = os.stat(SCAN_DIR + f)
+			file_details.append({
+				"name": f,
+				"size": stat.st_size,
+				"ts": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(stat.st_mtime)),
+				})
+
+	return template('scan', files=file_details)
+
+@route('/scans/<filename>')
+def convert(filename):
+	return static_file(filename, root=SCAN_DIR)
 
 @post('/scan')
 def scan():
+	global page
 	ppi = int(request.forms.get('ppi'))
 	mode = request.forms.get('mode')
 
 	cmd = "scanimage --format=tiff --resolution %d --mode %s" % (ppi, mode)
 	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, close_fds=True)
 
-	response.add_header('Content-type', 'image/gif')
+	stream = StringIO()
+	while True:
+		buf = p.stdout.read(1024)
+		if not buf: break
+		stream.write(buf)
+
+	img = Image.open(stream)
+	img.save(SCAN_DIR + 'page-%d.jpg' % page, 'JPEG', quality=95)
+	page += 1
+
+	redirect('/?xnav=scan')
+
+@route('/pdf')
+def pdf():
+	files = map(lambda x: SCAN_DIR + x, sorted(os.listdir(SCAN_DIR)))
+	cmd = "convert %s +compress pdf:-" % (str.join(' ', files))
+	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, close_fds=True)
+
+	response.add_header('Content-type', 'application/pdf')
 
 	while True:
 		buf = p.stdout.read(1024)
 		if not buf: break
 		yield buf
+
+@route('/scans/<filename>/delete')
+def delete(filename):
+	os.unlink(SCAN_DIR + filename)
+	redirect('/?xnav=deletefile')
+
+@route('/deleteall')
+def delete():
+	for f in os.listdir(SCAN_DIR):
+		os.unlink(SCAN_DIR + f)
+
+	redirect('/?xnav=delete')
 
 run(host='0.0.0.0', port=PORT, debug=True)
--- views/scan.tpl
+++ views/scan.tpl
@@ -9,6 +9,7 @@
 	<select name="ppi">
 		<option value="75">75 ppi</option>
 		<option value="100">100 ppi</option>
+		<option value="150">150 ppi</option>
 		<option value="300" selected>300 ppi</option>
 		<option value="600">600 ppi</option>
 	</select>
@@ -16,8 +17,27 @@
 	<label for="mode">Mode:</label>
 	<select name="mode">
 		<option value="Gray">Gray</option>
-		<option value="Color">Color</option>
+		<option value="Color" selected>Color</option>
 	</select>
 
 	<input type="submit" value="Scan" />
 </form>
+
+<h2>Images</h2>
+<a href="/pdf">Create PDF</a> | <a href="/deleteall" onclick="return confirm('Are you sure you wish to delete ALL files??? There is no undo!')"">Delete all</a><br />
+<table border="1" cellpadding="4" cellspacing="0">
+	<tr>
+		<th>Name</th>
+		<th>Size</th>
+		<th>Date</th>
+		<th>Actions</th>
+	</tr>
+	% for f in files:
+	<tr>
+		<td><a href="/scans/{{f['name']}}">{{f['name']}}</a></td>
+		<td>{{f['size']/1024}} KB</td>
+		<td>{{f['ts']}}</td>
+		<td><a href="/scans/{{f['name']}}/delete" onclick="return confirm('Are you sure you wish to delete {{f['name']}}?')">Delete</a></td>
+	</tr>
+	% end
+</table>
